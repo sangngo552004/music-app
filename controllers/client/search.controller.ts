ab697df2f7c5dd83e2f7ff90afcef9543fb27d0a
sangngo552004 <sangngo552004@gmail.com>
2024-03-23 10:52:34 +0700

    search function

---
+++ controllers/client/search.controller.ts
@@ -0,0 +1,43 @@
+import {Request, Response} from "express";
+import {convertToSlug} from "../../helpers/convert-to-slug.helper";
+import Song from "../../models/song.model";
+import Singer from "../../models/singer.model";
+
+//[GET] search/result
+export const result = async (req : Request, res : Response) => {
+    const keyword : string = `${req.query.keyword}`;
+
+    let songs = [];
+
+    if (result) {
+        const keywordRegex  = new RegExp (keyword, "i");
+
+        const slug = convertToSlug(keyword);
+
+        const keywordSlugRegex = new RegExp(slug, "i");
+
+        songs = await Song.find({
+            $or : [
+                {title : keywordRegex},
+                {slug : keywordSlugRegex}
+            ]
+        });
+
+        if (songs.length > 0) {
+            for (const song of songs) {
+                const infoSinger = await Singer.findOne({
+                    _id : song.singerId,
+                    deleted : false
+                });
+
+                song["infoSinger"] = infoSinger;
+            }
+        }
+    }
+
+    res.render("client/pages/search/result",{
+        pageTitle :`Kết quả : ${keyword}`,
+        keyword : keyword,
+        songs : songs
+    });
+}
